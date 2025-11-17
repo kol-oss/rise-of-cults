6a9a98de7f0698e1ec90b4ddd1528658da405eee
kol-oss <kolyakhozhajinov@gmail.com>
2025-11-17 20:41:08 +0200

    Rename project

--- src/main/java/com/github/koloss/ascension/AscensionPlugin.java
+++ src/main/java/com/github/koloss/ascension/AscensionPlugin.java
@@ -1,8 +1,8 @@
-package com.github.koloss.cults;
+package com.github.koloss.ascension;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class RiseOfCultsPlugin extends JavaPlugin {
+public final class AscensionPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
